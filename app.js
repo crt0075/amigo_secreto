f6204ab32e955e2262730d3e9ad863faf2271ab4
crt0075 <cristiantorres0075@gmail.com>
2025-03-18 11:05:13 -0600

    Version1_AmigoSecreto

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function asignarTextoElemento(elemento, texto) {
     }
 
 function agregarAmigo() {
-    let amigoingresado = document.getElementById("amigo").value;
+    let amigoingresado = document.getElementById("amigo").value.trim();
     if (amigoingresado === "") {
         alert("Debes ingresar un nombre");
         return;
@@ -22,6 +22,21 @@ function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
 
+function reiniciarListaAmigos() {
+    amigos = [];
+    asignarTextoElemento("ul", amigos);
+}
+
 function sortearAmigo() {
+  //  let amigoSorteado = Math.floor(Math.random() * amigos.length) + 1;
+      if (amigos.length === 0) {
+        alert("La lista de amigos está vacía");
+        return null;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    asignarTextoElemento("#resultado", `El amigo secreto es ${amigoSorteado}`);
+    reiniciarListaAmigos();
+    return amigoSorteado;
 }
 
